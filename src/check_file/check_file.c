c45e663ac745982ad3b55c00c5af576f05622875
Simon Abitbol <sabitbol@z2r3p8.42lyon.fr>
2024-10-15 20:27:09 +0200

    check_file

---
+++ src/check_file/check_file.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_file.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabitbol <sabitbol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 19:40:30 by sabitbol          #+#    #+#             */
+/*   Updated: 2024/10/15 20:26:43 by sabitbol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include.h"
+
+static t_bool  check_filename(char *file_name)
+{
+    int i;
+
+    i = 0;
+    while (file_name[i])
+        i++;
+    if (str[i - 1] != 't' || str[i - 2] != 'r' || str[i - 3] != '.')
+        return (true);
+    return (false);
+}
+
+int get_file(char *file_name)
+{
+    int fd;
+
+    if (check_filename(file_name))
+    {
+        ft_dprintf(2, "%s", E_FILE_EXT);
+        return (NULL);
+    }
+    fd = open(file_name, O_RDONLY);
+    if (fd == -1)
+    {
+        ft_dprintf(2, "%s", E_FILE_ACCESS);
+        return (NULL);
+    }
+    return (fd);
+}
